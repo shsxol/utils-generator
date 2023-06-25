729c1df3f01ab46595f40698521b4739e0e7b2dd
shsxol <shrestha.smile31@gmail.com>
2023-06-25 13:42:44 +0545

    express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express= require("express")
+
+
+const app = express()
+
+
+
+
+app.listen(8000, () => { // call back=function bhitra function call  garnu
+    console.log("server running in port 8000")
+
+});
