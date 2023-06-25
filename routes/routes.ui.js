4e654e939da8e1b2de4642ce1c72e210fc758ed5
shsxol <shrestha.smile31@gmail.com>
2023-06-25 15:03:26 +0545

    express currency converter

--- routes/routes.ui.js
+++ routes/routes.ui.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
-
+const CC= require("currency-converter-lt");
 
 router.get("/",(req,res) =>{
     res.send({msg: "Hello from UI"});
+});
+
+router.get("/converter/:currency1/:currency2/:value", async(req,res) =>{
+    const { currency1, currency2, value} = req.params;
+
+    
+
+    
+        const currencyConverter = new CC ({
+            from: currency1,
+            to: currency2,
+            amount: Number(value)
+        });
+
+        const result = await currencyConverter.convert();
+
+
+    res.send(`hello ${result}`);
+
 });
 
 
