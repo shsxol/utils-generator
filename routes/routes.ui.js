69950be9958e84da2a1c75ee1aa43a10c92e649a
shsxol <shrestha.smile31@gmail.com>
2023-06-27 14:55:49 +0545

    contact page

--- routes/routes.ui.js
+++ routes/routes.ui.js
@@ -7,6 +7,10 @@ router.get("/",(req,res) =>{
     res.render("index");
 });
 
+router.get("/contact",(req,res) =>{
+    res.render("contact");
+});
+
 router.get("/converter/:currency1/:currency2/:value",async(req,res) =>{
     const { currency1, currency2, value} = req.params;
 
